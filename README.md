afd15c58cbdfecf8612eafe5c1bad2ae99640cb9
wuyang910217 <wuyang910217@gmail.com>
2019-05-09 15:43:59 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# nodejs开发第三方库模板
+
+## 说明
+
+- 在`webpack.config.js`里添加库的名称
+- 在`package.json`里设置项目名、作者等
+- 设置`debug`库的log前缀 如`DEBUG=wuyang*`
+- 在`example/`目录下 添加使用的例子
+- 执行`npm run prepublish`发布你的库包
+- 发布npm包时默认是没有包括`lib/`目录下的源代码 开启步骤
+  - `package.json`文件files字段添加`lib`
+  - `.npmignore`文件注释`lib`字段
+- 涉及密码等隐私内容 请参考`.env-demo`文件
+
+## 依赖
+
+项目依赖以下库，可以在你的程序里直接使用，不用再次安装
+
+- debug
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const webpackConfig = {
+  mode: process.env.NODE_ENV,
+  entry: ['./lib/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    sourceMapFilename: 'index.map',
+    library: '', //设置库的名称
+    libraryTarget: 'umd',
+  },
+  devtool: 'source-map',
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.js'],
+  },
+};
+
+if (isDev) {
+  webpackConfig.plugins.push(new NodemonPlugin());
+}
+
+module.exports = webpackConfig;
