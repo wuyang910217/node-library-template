25e03cdc90d1375ecf382829d136dae8ea039359
wuyang910217 <wuyang910217@gmail.com>
2019-05-09 21:22:27 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ const webpackConfig = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
     ],
